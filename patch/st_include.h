651bf63176fecff5168d47233d9d577c0f1c3883
julmajustus <julmajustus@tutanota.com>
2023-08-03 21:23:54 +0300

    Initial checkout

---
+++ patch/st_include.h
@@ -0,0 +1,6 @@
+/* Patches */
+#include "openurlonclick.h"
+#include "scrollback.h"
+// #if VIM_BROWSE_PATCH
+// #include "normalMode.h"
+// #endif
---
+++ patch/x_include.h
@@ -0,0 +1,4 @@
+/* Patches */
+#include "boxdraw.h"
+#include "font2.h"
+#include "xresources.h"
